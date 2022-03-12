4ba26b8317d7c3f19fcb1ea204ce4667afe391c1
Dominic Bilot <bilot.dcabpray@gmail.com>
2022-03-11 19:36:21 -0600

    installed vector inclusion

--- source.cpp
+++ source.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    cout << "Hello World";
+    string name;
+    cout << "Hello World" << endl << "Enter Name: ";
+    cin >> name;
+    cout << name;
 }
