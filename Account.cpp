825bdefc86a548f272a0c4de03c7f82ef37ecf7b
Dakota Stephens <kota.helives@gmail.com>
2022-03-21 01:47:13 -0500

    Inital Character Stat setup is working.  Now we need to set it up so that we send info to the server which is stored within a file.  Also we need to setup a safety net for players who loose connection so that they can run through this again if they loose connection and did not complete it.

--- Account.cpp
+++ Account.cpp
@@ -5,28 +5,89 @@
 
 using namespace std;
 
-void Account::setHealth(){
+/*************************************************************************
+The purpose of this file to to manage the user account and Character setup
+*************************************************************************/
+
+
+
+void Account::setHealth(){ //inital choise of health stats
+    int healthS;
     cout << "Enter the number of points you would like to add to your Health stat: ";
-    cin >> health;
+    cin >> healthS;
+    if (healthS <= remainingStatPoints) {
+        health = healthS;
+        remainingStatPoints -= healthS;
+    } else {
+        cout << "You entered an invalid number of statpoint to be added." << endl << "Restarting this proccess so you can complete this task.";
+        health = attack = armor = magicResistance = 0;
+        createCharacter();
+    }
 }
-void Account::setAttack(){
-    cout << "Enter the number of points you would like to add to your Health stat: ";
-    cin >> attack;
+void Account::setAttack(){ //inistal choice of Attack stats
+    int attackS;
+    cout << "Enter the number of points you would like to add to your Attack stat: ";
+    cin >> attackS;
+    if (attackS <= remainingStatPoints) {
+        attack = attackS;
+        remainingStatPoints -= attackS;
+    } else {
+        cout << "You entered an invalid number of statpoint to be added." << endl << "Restarting this proccess so you can complete this task.";
+        health = attack = armor = magicResistance = 0;
+        createCharacter();
+    }
 }
-void Account::setArmor(){
-    cout << "Enter the number of points you would like to add to your Health stat: ";
-    cin >> armor;
+void Account::setArmor(){ //initial choice of Armor stats
+    int armorS;
+    cout << "Enter the number of points you would like to add to your Armor stat: ";
+    cin >> armorS;
+    if (armorS <= remainingStatPoints) {
+        armor = armorS;
+        remainingStatPoints -= armorS;
+    } else {
+        cout << "You entered an invalid number of statpoint to be added." << endl << "Restarting this proccess so you can complete this task.";
+        health = attack = armor = magicResistance = 0;
+        createCharacter();
+    }
 }
-void Account::setMagicResistance(){
-    cout << "Enter the number of points you would like to add to your Health stat: ";
-    cin >> magicResistance;
+void Account::setMagicResistance(){ //initial choice of Magic Resistance stats
+    int magicResistanceS;
+    cout << "Enter the number of points you would like to add to your Magic Resistance stat: ";
+    cin >> magicResistanceS;
+    if (magicResistanceS <= remainingStatPoints) {
+        magicResistance = magicResistanceS;
+        remainingStatPoints -= magicResistanceS;
+    } else {
+        cout << "You entered an invalid number of statpoint to be added." << endl << "Restarting this proccess so you can complete this task.";
+        health = attack = armor = magicResistance = 0;
+        createCharacter();
+    }
 }
 
-void Account::createCharacter(){
+void logonScreen(int type = 1); //this allows us to use the logon screen function at the end of this function and pass in the defualt value of 1
+void Account::createCharacter(){ //running through the stat choosing - This is the inital user setup
+    remainingStatPoints = 3;
     system("cls");
-    cout << "(1ST - Health, 2ND - Attack, 3RD - Armor, 4TH - Magic Resistance)" << endl << "You have 3 points that can be added to the above stats" << endl << "Please enter the number of points to add to each stat: " << endl;
+    cout << "(1ST - Health, 2ND - Attack, 3RD - Armor, 4TH - Magic Resistance)" << endl << "You have 3 points that can be distributed among the above stats" << endl;
+    system("pause");
+    system("cls");
+    cout << "(1ST - Health, 2ND - Attack, 3RD - Armor, 4TH - Magic Resistance)" << endl << "You have 3 points that can be distributed among the above stats" << endl << endl << "Please enter the number of points to add to each stat." << endl << endl;
     setHealth();
-    setAttack();
-    setArmor();
-    setMagicResistance();
+    if (remainingStatPoints > 0) setAttack();
+    if (remainingStatPoints > 0) setArmor();
+    if (remainingStatPoints > 0) setMagicResistance();
+    if (remainingStatPoints > 0) { //if the user did not use all their stat points we will restart the proccess
+        cout << remainingStatPoints << endl;
+        system("cls");
+        cout << "You did not use all your Stat Points..." << endl << "Restarting this proccess so you can complete this task.";
+        system("pause");
+        health = attack = armor = magicResistance = 0; //reset all to 0
+        createCharacter();
+    }
+    //Need to write the stats to file so that they are stored to be opened again later.
+    //if the code makes it to this point then the user is done and will go to main menu.
+    cout << "Your stat points have been to set to the following: " << endl << "Health: " << health << endl << "Attack: " << attack << endl << "Armor: " << armor << endl << "Magic Resistance: " << magicResistance << endl;
+    // HasGoneThroughInitalSetup = true; //sets the value to true because the user has successfully added the 3 inital setup points to their Character.
+    system("pause");
+    logonScreen();
 }
--- Account.h
+++ Account.h
@@ -6,7 +6,9 @@ using namespace std;
 
 class Account {
     public:
+        bool HasGoneThroughInitalSetup; //this will be used to make sure they have added the inital 3 stat points - This needs to be defualted to false in the user account creation on the server.
         string username;
+        int remainingStatPoints;
         int health, attack, armor, magicResistance;
         void setHealth();
         void setAttack();
