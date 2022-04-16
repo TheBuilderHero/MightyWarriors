dd020c84289ced6a3bc45bef0601dbb8f438b63f
Dakota Stephens <kota.helives@gmail.com>
2022-04-16 06:12:56 -0500

    making class variables private

--- Account.h
+++ Account.h
@@ -3,11 +3,12 @@
 #pragma once
 
 class Account {
+    private:
+    bool HasGoneThroughInitalSetup; //this will be used to make sure they have added the inital 3 stat points - This needs to be defualted to false in the user account creation on the server.
+    std::string username;
+    int remainingStatPoints;
+    int initHealth = 0, initArmor = 0, initMagicResistance = 0, initPhysicalDamage = 0, initMagicDamage = 0, initAgility = 0, initStealth = 0, initStamina = 0, initMana = 0;
     public:
-        bool HasGoneThroughInitalSetup; //this will be used to make sure they have added the inital 3 stat points - This needs to be defualted to false in the user account creation on the server.
-        std::string username;
-        int remainingStatPoints;
-        int initHealth = 0, initArmor = 0, initMagicResistance = 0, initPhysicalDamage = 0, initMagicDamage = 0, initAgility = 0, initStealth = 0, initStamina = 0, initMana = 0;
         void setHealth(std::string username);
         void setArmor(std::string username);
         void setMagicResistance(std::string username);
--- ReachOutToServer.h
+++ ReachOutToServer.h
@@ -4,10 +4,12 @@
 #pragma once
 
 class ReachOutToServer{
-public:
+private:
     std::string message;
     char buf[4096];
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+public:
     std::string sendToServer(std::string aMessage);
 
 };
