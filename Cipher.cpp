be82974d8a190778ba81b32c7a07ffa212ae6fa1
Dakota Stephens <kota.helives@gmail.com>
2022-06-15 01:06:52 -0500

    Added subCipher & subDecipher functionality

--- Cipher.cpp
+++ Cipher.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 //this function is intended to pull out the delimiters from a message and the data associated with each request
-string Cipher::decipher(char messageFromClient[]){
+string Cipher::decipher(char messageFromClient[], bool hasSubItems){
     
     //string message = messageFromClient; // change the message into a string
 
@@ -27,67 +27,218 @@ string Cipher::decipher(char messageFromClient[]){
             if (output.length() > 0) responseType = stoi(output); // we many need to change the variable to an int with stoi(output) later but right now we just want a string version
             break;
             case 2://the second item enclosed in delimiters
-            if (output.length() > 0) item2 = output; 
+            if (output.length() > 0) {
+                item[2] = output;
+                if (hasSubItems) subDecipher(item[2], 2);
+                } 
             break;
-            case 3://the third item enclosed in delimiters
-            if (output.length() > 0) item3 = output; 
+            case 3://third item after delimiter
+            if (output.length() > 0) {
+                item[3] = output;
+                if (hasSubItems) subDecipher(item[3], 3);
+                }
             break;
-            case 4://the forth item enclosed in delimiters
-            if (output.length() > 0) item4 = output; 
+            case 4://forth item after delimiter
+            if (output.length() > 0) {
+                item[4] = output;
+                if (hasSubItems) subDecipher(item[4], 4);
+                }
             break;
-            case 5://the fith item enclosed in delimiters
-            if (output.length() > 0) item5 = output; 
+            case 5://fith item after delimiter
+            if (output.length() > 0) {
+                item[5] = output;
+                if (hasSubItems) subDecipher(item[5], 5);
+                }
             break;
-            case 6://the sixth item enclosed in delimiters
-            if (output.length() > 0) item6 = output; 
+            case 6://sixth item after delimiter
+            if (output.length() > 0) {
+                item[6] = output;
+                if (hasSubItems) subDecipher(item[6], 6);
+                }
             break;
-            case 7://the seventh item enclosed in delimiters
-            if (output.length() > 0) item7 = output; 
+            case 7://seventh item after delimiter
+            if (output.length() > 0) {
+                item[7] = output;
+                if (hasSubItems) subDecipher(item[7], 7);
+                }
             break; 
             case 8://the eighth item enclosed in delimiters
-            if (output.length() > 0) item8 = output; 
+            if (output.length() > 0) {
+                item[8] = output;
+                if (hasSubItems) subDecipher(item[8], 8);
+                } 
             break;
             case 9://the ninth item enclosed in delimiters
-            if (output.length() > 0) item9 = output; 
+            if (output.length() > 0) {
+                item[9] = output;
+                if (hasSubItems) subDecipher(item[9], 9);
+                } 
             break;
             case 10://the tenth item enclosed in delimiters
-            if (output.length() > 0) item10 = output; 
+            if (output.length() > 0) {
+                item[10] = output;
+                if (hasSubItems) subDecipher(item[10], 10);
+                } 
             break;
-            case 11://the eleventh item enclosed in delimiters
-            if (output.length() > 0) item11 = output; 
+            case 11://the tenth item enclosed in delimiters
+            if (output.length() > 0) {
+                item[11] = output;
+                if (hasSubItems) subDecipher(item[11], 11);
+                } 
             break;
-            case 12://the twelth item enclosed in delimiters
-            if (output.length() > 0) item12 = output; 
+            case 12://the tenth item enclosed in delimiters
+            if (output.length() > 0) {
+                item[12] = output;
+                if (hasSubItems) subDecipher(item[12], 12);
+                } 
             break;
             case 13:
-            if (output.length() > 0) item13 = output; 
+            if (output.length() > 0) {
+                item[13] = output;
+                if (hasSubItems) subDecipher(item[13], 13);
+                } 
             break;
             case 14:
-            if (output.length() > 0) item14 = output; 
+            if (output.length() > 0) {
+                item[14] = output;
+                if (hasSubItems) subDecipher(item[14], 14);
+                } 
             break;
             case 15:
-            if (output.length() > 0) item15 = output; 
+            if (output.length() > 0) {
+                item[15] = output;
+                if (hasSubItems) subDecipher(item[15], 15);
+                } 
             break;
             case 16:
-            if (output.length() > 0) item16 = output; 
+            if (output.length() > 0) {
+                item[16] = output;
+                if (hasSubItems) subDecipher(item[16], 16);
+                } 
             break;
             case 17:
-            if (output.length() > 0) item17 = output; 
+            if (output.length() > 0) {
+                item[17] = output;
+                if (hasSubItems) subDecipher(item[17], 17);
+                } 
             break;
             case 18:
-            if (output.length() > 0) item18 = output; 
+            if (output.length() > 0) {
+                item[18] = output;
+                if (hasSubItems) subDecipher(item[18], 18);
+                } 
             break;
             case 19:
-            if (output.length() > 0) item19 = output; 
+            if (output.length() > 0) {
+                item[19] = output;
+                if (hasSubItems) subDecipher(item[19], 19);
+                } 
             break;
             case 20:
-            if (output.length() > 0) item20 = output; 
+            if (output.length() > 0) {
+                item[20] = output;
+                if (hasSubItems) subDecipher(item[20], 20);
+                } 
             break;
             case 21:
-            if (output.length() > 0) item21 = output; 
+            if (output.length() > 0) {
+                item[21] = output;
+                if (hasSubItems) subDecipher(item[21], 21);
+                } 
             break;
             case 22:
-            if (output.length() > 0) item22 = output; 
+            if (output.length() > 0) {
+                item[22] = output;
+                if (hasSubItems) subDecipher(item[22], 22);
+                } 
+            break;
+        }
+        loopPass++;
+    }
+    return str_file_content;
+}
+string Cipher::subDecipher(string subMessageFromClient, int rootItem){
+    
+    //string message = messageFromClient; // change the message into a string
+
+    
+    // the above variables may later be replaced with a more wide veriety of variables however, for testing we are using all strings
+    string s = subMessageFromClient;
+    string str_file_content;
+    string token, output;
+    int loopPass = 0;
+    size_t pos = 0; // position variable for removing the subDelimiters to view the message
+    while ((pos = s.find(subDelimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        output = token;
+        str_file_content += std::string(token); // we do not need to add spaces between the information for now so I removed: + std::string(" ")
+        s.erase(0, pos + subDelimiter.length());
+        switch (loopPass){
+            case 1://the first item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][1] = output; // we many need to change the variable to an int with stoi(output) later but right now we just want a string version
+            break;
+            case 2://the second item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][2] = output; 
+            break;
+            case 3://the third item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][3] = output; 
+            break;
+            case 4://the forth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][4] = output; 
+            break;
+            case 5://the fith item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][5] = output; 
+            break;
+            case 6://the sixth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][6] = output; 
+            break;
+            case 7://the seventh item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][7] = output; 
+            break; 
+            case 8://the eighth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][8] = output; 
+            break;
+            case 9://the ninth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][9] = output; 
+            break;
+            case 10://the tenth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][10] = output; 
+            break;
+            case 11://the eleventh item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][11] = output; 
+            break;
+            case 12://the twelth item enclosed in subDelimiters
+            if (output.length() > 0) subItem[rootItem][12] = output; 
+            break;
+            case 13:
+            if (output.length() > 0) subItem[rootItem][13] = output; 
+            break;
+            case 14:
+            if (output.length() > 0) subItem[rootItem][14] = output; 
+            break;
+            case 15:
+            if (output.length() > 0) subItem[rootItem][15] = output; 
+            break;
+            case 16:
+            if (output.length() > 0) subItem[rootItem][16] = output; 
+            break;
+            case 17:
+            if (output.length() > 0) subItem[rootItem][17] = output; 
+            break;
+            case 18:
+            if (output.length() > 0) subItem[rootItem][18] = output; 
+            break;
+            case 19:
+            if (output.length() > 0) subItem[rootItem][19] = output; 
+            break;
+            case 20:
+            if (output.length() > 0) subItem[rootItem][20] = output; 
+            break;
+            case 21:
+            if (output.length() > 0) subItem[rootItem][21] = output; 
+            break;
+            case 22:
+            if (output.length() > 0) subItem[rootItem][22] = output; 
             break;
         }
         loopPass++;
@@ -176,6 +327,9 @@ string Cipher::decipherS(string messageFromClient){
             case 21:
             if (output.length() > 0) itemS21 = output; 
             break;
+            case 22:
+            if (output.length() > 0) itemS22 = output; 
+            break;
         }
         loopPass++;
     }
@@ -183,9 +337,9 @@ string Cipher::decipherS(string messageFromClient){
 }
 
 //this functions purpose it to add the delimiters to given items 
-string Cipher::cipher(string typeOfRequest, string username, string item3, string item4, string item5, string item6, string item7, string item8, string item9, string item10, string item11, string item12){ // the default values have been set to "" in case no input is given
+string Cipher::cipher(string typeOfRequest, string username, string item3, string item4, string item5, string item6, string item7, string item8, string item9, string item10, string item11, string item12, string item13, string item14, string item15, string item16, string item17, string item18, string item19, string item20, string item21, string item22){ // the default values have been set to "" in case no input is given
     int numberOfItems = 12; //max number of items that we can cipher
-    string delimiter = "~"; //a character that marks the beginning or end of a unit of data
+    //"~" //a character that marks the beginning or end of a unit of data
 
     string str_file_content;
     int loopPass = 1; // start the loop at 1 so that there is no extra spaces
@@ -228,6 +382,36 @@ string Cipher::cipher(string typeOfRequest, string username, string item3, strin
             case 12:
             if (item12.length() > 0) str_file_content += item12;
             break;
+            case 13:
+            if (item13.length() > 0) str_file_content += item13;
+            break;
+            case 14:
+            if (item14.length() > 0) str_file_content += item14;
+            break;
+            case 15:
+            if (item15.length() > 0) str_file_content += item15;
+            break;
+            case 16:
+            if (item16.length() > 0) str_file_content += item16;
+            break;
+            case 17:
+            if (item17.length() > 0) str_file_content += item17;
+            break;
+            case 18:
+            if (item18.length() > 0) str_file_content += item18;
+            break;
+            case 19:
+            if (item19.length() > 0) str_file_content += item19;
+            break;
+            case 20:
+            if (item20.length() > 0) str_file_content += item20;
+            break;
+            case 21:
+            if (item21.length() > 0) str_file_content += item21;
+            break;
+            case 22:
+            if (item22.length() > 0) str_file_content += item22;
+            break;
         }
         loopPass++;
     }
@@ -235,77 +419,91 @@ string Cipher::cipher(string typeOfRequest, string username, string item3, strin
     return str_file_content;
 }
 
-string Cipher::getItem(int itemNumberToReturn){
-    switch (itemNumberToReturn)
-    {
-    case 2:
-        return item2;
-        break;
-    case 3:
-        return item3;
-        break;
-    case 4:
-        return item4;
-        break;
-    case 5:
-        return item5;
-        break;
-    case 6:
-        return item6;
-        break;
-    case 7:
-        return item7;
-        break;
-    case 8:
-        return item8;
-        break;
-    case 9:
-        return item9;
-        break;
-    case 10:
-        return item10;
-        break;
-    case 11:
-        return item11;
-        break;
-    case 12:
-        return item12;
-        break;
-    case 13:
-        return item13;
-        break;
-    case 14:
-        return item14;
-        break;
-    case 15:
-        return item15;
-        break;
-    case 16:
-        return item16;
-        break;
-    case 17:
-        return item17;
-        break;
-    case 18:
-        return item18;
-        break;
-    case 19:
-        return item19;
-        break;
-    case 20:
-        return item20;
-        break;
-    case 21:
-        return item21;
-        break;
-    case 22:
-        return item22;
-        break;
-    
-    default:
-    return "Default Output Given";
-        break;
+string Cipher::subCipher(string item1, string item2, string item3, string item4, string item5, string item6, string item7, string item8, string item9, string item10, string item11, string item12, string item13, string item14, string item15, string item16, string item17, string item18, string item19, string item20, string item21, string item22){ // the default values have been set to "" in case no input is given
+    int numberOfItems = 12; //max number of items that we can cipher
+    //"=" //a character that marks the beginning or end of a unit of data
+
+    string str_file_content;
+    int loopPass = 1; // start the loop at 1 so that there is no extra spaces
+    while (loopPass <= numberOfItems) {
+        str_file_content += subDelimiter; // this will add the seperating subDelimiter before the a given item
+        switch (loopPass){
+            case 1:
+            if (item1.length() > 0) str_file_content += item1;
+            break;
+            case 2:
+            if (item2.length() > 0) str_file_content += item2;
+            break;
+            case 3:
+            if (item3.length() > 0) str_file_content += item3;
+            break;
+            case 4:
+            if (item4.length() > 0) str_file_content += item4;
+            break;
+            case 5:
+            if (item5.length() > 0) str_file_content += item5;
+            break;
+            case 6:
+            if (item6.length() > 0) str_file_content += item6;
+            break;
+            case 7:
+            if (item7.length() > 0) str_file_content += item7;
+            break;
+            case 8:
+            if (item8.length() > 0) str_file_content += item8;
+            break;
+            case 9:
+            if (item9.length() > 0) str_file_content += item9;
+            break;
+            case 10:
+            if (item10.length() > 0) str_file_content += item10;
+            break;
+            case 11:
+            if (item11.length() > 0) str_file_content += item11;
+            break;
+            case 12:
+            if (item12.length() > 0) str_file_content += item12;
+            break;
+            case 13:
+            if (item13.length() > 0) str_file_content += item13;
+            break;
+            case 14:
+            if (item14.length() > 0) str_file_content += item14;
+            break;
+            case 15:
+            if (item15.length() > 0) str_file_content += item15;
+            break;
+            case 16:
+            if (item16.length() > 0) str_file_content += item16;
+            break;
+            case 17:
+            if (item17.length() > 0) str_file_content += item17;
+            break;
+            case 18:
+            if (item18.length() > 0) str_file_content += item18;
+            break;
+            case 19:
+            if (item19.length() > 0) str_file_content += item19;
+            break;
+            case 20:
+            if (item20.length() > 0) str_file_content += item20;
+            break;
+            case 21:
+            if (item21.length() > 0) str_file_content += item21;
+            break;
+            case 22:
+            if (item22.length() > 0) str_file_content += item22;
+            break;
+        }
+        loopPass++;
     }
+    str_file_content += subDelimiter; // this will add the seperating subDelimiter after all the data
+    return str_file_content;
+}
+
+string Cipher::getItem(int itemNumberToReturn, int subItemNumber){ //this is usally item3 - item22 However, the subItemNumbers are item1 - item22
+    if(subItemNumber > 0) return subItem[itemNumberToReturn][subItemNumber];
+    return item[itemNumberToReturn];
 }
 
 string Cipher::getItemS(int itemNumberToReturn){
@@ -373,6 +571,9 @@ string Cipher::getItemS(int itemNumberToReturn){
         break;
     case 21:
         return itemS21;
+        break;
+    case 22:
+        return itemS22;
         break;
     
     default:
--- Cipher.h
+++ Cipher.h
@@ -5,20 +5,25 @@
 class Cipher {
     private:
     std::string delimiter = "~"; //a character that marks the beginning or end of a unit of data
+    std::string subDelimiter = "="; //a character that marks the beginning or end of a unit of data
     int responseType;
-    std::string item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22; // declare the variables that are being used to store the message from the client - some use item1
-    std::string itemS1, itemS2, itemS3, itemS4, itemS5, itemS6, itemS7, itemS8, itemS9, itemS10, itemS11, itemS12, itemS13, itemS14, itemS15, itemS16, itemS17, itemS18, itemS19, itemS20, itemS21; //these variables are for the use of decipherS so that the cipher and decipher variables do not get mixed up with it.
+    std::string item[22];//item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22; // declare the variables that are being used to store the message from the client - some use item1
+    std::string subItem[22][22];
+    std::string itemS1, itemS2, itemS3, itemS4, itemS5, itemS6, itemS7, itemS8, itemS9, itemS10, itemS11, itemS12, itemS13, itemS14, itemS15, itemS16, itemS17, itemS18, itemS19, itemS20, itemS21, itemS22; //these variables are for the use of decipherS so that the cipher and decipher variables do not get mixed up with it.
 
     public:
     
-    std::string decipher(char messageFromClient[]);//this function is intended to pull out the delimiters from a message and the data associated with each request
+    std::string decipher(char messageFromClient[], bool hasSubItems = false);//this function is intended to pull out the delimiters from a message and the data associated with each request
+    std::string subDecipher(std::string subMessageFromClient, int rootItem);//this function is intended to pull out the subDelimiters from a message and the data
     std::string decipherS(std::string messageFromClient);//this function is intended to pull out the delimiters from a message and the data associated with each request - string version
 
-    std::string cipher(std::string typeOfRequest, std::string username= "", std::string item3= "", std::string item4= "", std::string item5= "", std::string item6= "", std::string item7= "", std::string item8= "", std::string item9= "", std::string item10= "", std::string item11= "", std::string item12= "");// the default values have been set to "" in case no input is given
+    std::string cipher(std::string typeOfRequest, std::string username= "", std::string item3= "", std::string item4= "", std::string item5= "", std::string item6= "", std::string item7= "", std::string item8= "", std::string item9= "", std::string item10= "", std::string item11= "", std::string item12= "", std::string item13= "", std::string item14= "", std::string item15= "", std::string item16= "", std::string item17= "", std::string item18= "", std::string item19= "", std::string item20= "", std::string item21= "", std::string item22= "");// the default values have been set to "" in case no input is given
+
+    std::string subCipher(std::string typeOfRequest, std::string username= "", std::string item3= "", std::string item4= "", std::string item5= "", std::string item6= "", std::string item7= "", std::string item8= "", std::string item9= "", std::string item10= "", std::string item11= "", std::string item12= "", std::string item13= "", std::string item14= "", std::string item15= "", std::string item16= "", std::string item17= "", std::string item18= "", std::string item19= "", std::string item20= "", std::string item21= "", std::string item22= "");// the default values have been set to "" in case no input is given
 
     //return for the private variables:
     std::string getDelimiter() { return delimiter; }
     int getResponseType() { return responseType; }
-    std::string getItem(int itemNumberToReturn);
+    std::string getItem(int itemNumberToReturn, int subItemNumber = 0); //default is 0 meaning that it is not considered to have subItems
     std::string getItemS(int itemNumberToReturn);
 };
