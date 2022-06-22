779f667751ae27a37a4afb00d3c96c7b075e976a
Dakota Stephens <kota.helives@gmail.com>
2022-06-22 00:30:55 -0500

    Still not successful on console close triggering saving of all user data

--- source.cpp
+++ source.cpp
@@ -18,62 +18,24 @@ const int gamePatch       = menu.gamePatch;
 
 using namespace std; //so I dont have to type "std::"
 
-BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
-{
-    switch (fdwCtrlType)
-    {
-        // Handle the CTRL-C signal.
-    case CTRL_C_EVENT:
-        printf("Ctrl-C event\n\n");
-        Beep(750, 300);
-        return TRUE;
-
-        // CTRL-CLOSE: confirm that the user wants to exit.
-    case CTRL_CLOSE_EVENT:
-        Beep(600, 200);
-        printf("Ctrl-Close event\n\n");
-        return TRUE;
-
-        // Pass other signals to the next handler.
-    case CTRL_BREAK_EVENT:
-        Beep(900, 200);
-        printf("Ctrl-Break event\n\n");
-        return FALSE;
-
-    case CTRL_LOGOFF_EVENT:
-        Beep(1000, 200);
-        printf("Ctrl-Logoff event\n\n");
-        return FALSE;
-
-    case CTRL_SHUTDOWN_EVENT:
-        Beep(750, 500);
-        printf("Ctrl-Shutdown event\n\n");
-        return FALSE;
-
-    default:
-        return FALSE;
-    }
+void fnExit(){
+    std::cout << "You are closing the application..." << std::endl;
+    std::cout << "1" << std::endl;
+    std::cout << "2" << std::endl;
+    std::cout << "3" << std::endl;
+    std::cout << "4" << std::endl;
+    std::cout << "5" << std::endl;
+    std::cout << "6" << std::endl;
+    std::cout << "7" << std::endl;
+    std::cout << "8" << std::endl;
+    std::cout << "9" << std::endl;
+    std::cout << "10" << std::endl;
+    std::cout << "end" << std::endl;
+    std::cout << "this funtion is incomplete but will be used to save all userdata before closing the aplication." << std::endl;
+    system("pause");
 }
 
 int main(){ //Starts the program if the clients version is compatable with the server's versions
-
-    //This is another test of apllication closing ability with saving user data:
-    /*if (SetConsoleCtrlHandler(CtrlHandler, TRUE)) //controls exit
-    {
-        printf("\nThe Control Handler is installed.\n");
-        printf("\n -- Now try pressing Ctrl+C or Ctrl+Break, or");
-        printf("\n    try logging off or closing the console...\n");
-        printf("\n(...waiting in a loop for events...)\n\n");
-
-        while (1) {}
-    }
-    else
-    {
-        printf("\nERROR: Could not set control handler");
-        return 1;
-    }*/
-
-
     string answer;
     ReachOutToServer server; //declare a new instance of ReachOutToServer called server
     Account account; //declare a new instance of the Account class
